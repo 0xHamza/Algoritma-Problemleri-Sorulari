8c68473145774567cc59c2f787c5abee0134ce22
0xHamza <43hamzacelik@gmail.com>
2022-11-23 21:40:54 +0300

    new problems adding

---
+++ Coderbyte/DiffrentCase to CamelCase.py
@@ -0,0 +1,33 @@
+def DifferentCases(strParam):
+
+  newStr = ''
+  finalWord = []
+    
+  for char in strParam:
+    if char.isalpha():
+      newStr += char
+    else:
+      newStr += ' '
+  
+  for word in newStr.split():
+    l = list(word)
+    l[0] = l[0].upper()
+    for i in range(1,len(l)):
+      l[i] = l[i].lower()
+    s="".join(l)
+    finalWord.append(s)
+  
+
+  bas = list(finalWord[0])
+  bas[0] = bas[0].lower()
+  finalWord[0] = "".join(bas)
+
+  return ''.join(finalWord)
+
+# keep this function call here 
+print(DifferentCases("BOB loves-coding,for*ever"))
+
+"""
+inputs:
+"BOB loves-coding,for*ever"
+"""
