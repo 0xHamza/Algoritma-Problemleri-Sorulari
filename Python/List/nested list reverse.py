28a070365a60e800346bd1918d4b29232bdbd5e3
0xHamza <43hamzacelik@gmail.com>
2022-11-23 18:01:09 +0300

    update

--- Python/List/nested list reverse.py
+++ Python/List/nested list reverse.py
@@ -1,17 +1,26 @@
 '''
 Author: 0xHamza
 Subject: Nested List Reverse
+Description: 'İç içe olan listeye içlerindeki elemanlarıyla berabver terse çevirme.'
 Date: 23.11.2022
 Source:
 	-https://app.patika.dev/courses/python-temel/proje
 '''
 
+
+def deep_reverse(mylist):                  
+    res = []                                #Tersledigimiz listeleri tutacagimiz degisken
+    for e in mylist:                        #Ic ice listelerin icine girilir
+        if isinstance(e, list):             #Eger elemean bir list ise, nested list yapisi isE:
+            res.append(deep_reverse(e))     #Recursive bir yapi ile o listenin icindeki elemanlari da sonuc degiskenine al
+        else:                               #Nested list yapisi yoksa yani ic ice bir liste degilse:
+            res.append(e)                   #Elemani sonuc degiskenine al
+    res.reverse()                           #Alinan elemanları tersle
+    return res                              
+
+
 input = [[1, 2], [3, 4, [11,12]], [5, 6, 7]]
-input.reverse()
+input_reversed = deep_reverse(input);
 
-print(input)
-for i in range(len(input)):
-    print("a")
-    (input[i])=(input[i])[::-1]
-
-print(input)
+print("Original: ",input)
+print("Reversed: ",input_reversed)
